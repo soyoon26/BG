9a0d4d88047614e153e6abd4cc288c2efd369641
soyoon26 <barcardi26@gmail.com>
2024-01-19 15:47:31 +0900

    add MainPage

--- frontend/bg/src/pages/MainPage.js
+++ frontend/bg/src/pages/MainPage.js
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const MainPage = () => {
   return (
-    <div className=" text-3xl">
-      <div>Main Page</div>
+    <div>
+      <div className="flex">
+        <Link to={"./about"}>About</Link>
+        {/* 클릭시 추가적 로딩 */}
+      </div>
+      <div className="text-3xl">Main Page</div>
     </div>
   );
 };
