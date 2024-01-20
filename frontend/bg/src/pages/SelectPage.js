9529d8c96972a7f34ea1d94633164135db575899
soyoon26 <barcardi26@gmail.com>
2024-01-20 21:28:36 +0900

    add setup background

--- frontend/bg/src/pages/SelectPage.js
+++ frontend/bg/src/pages/SelectPage.js
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import background from "../images/setup_background.png";
 
 const SelectPage = () => {
+  const backStyles = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    height: "100vh",
+  };
+
   return (
-    <div>
+    <div style={backStyles}>
       <div className="flex">
         <Link to={"../about"}>About</Link>
         {/* 클릭시 추가적 로딩 */}
       </div>
+      {/* <img src={background} alt="설정배경" /> */}
       <div className="text-3xl">게임 선택하기 화면 </div>
     </div>
   );
