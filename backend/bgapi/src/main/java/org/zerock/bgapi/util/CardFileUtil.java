50197b16b3288b6d7fb4aef48ac73fa74f83b800
soyoon26 <barcardi26@gmail.com>
2024-01-25 17:18:05 +0900

    update gradle

---
+++ backend/bgapi/src/main/java/org/zerock/bgapi/util/CardFileUtil.java
@@ -0,0 +1,26 @@
+package org.zerock.bgapi.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.log4j.Log4js;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+@Log4j2 //로깅
+@RequiredArgsConstructor
+public class CardFileUtil {
+    
+}
