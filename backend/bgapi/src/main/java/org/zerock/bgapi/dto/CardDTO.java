93359daafe703527448819aaabe2c8ec1110df51
soyoon26 <barcardi26@gmail.com>
2024-01-25 17:00:38 +0900

    update DTO

---
+++ backend/bgapi/src/main/java/org/zerock/bgapi/dto/CardDTO.java
@@ -0,0 +1,23 @@
+package org.zerock.bgapi.dto;
+import lombok.*;
+import java.util.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardDTO {
+    private Long pno;
+    private String pname;
+    private int price;
+    private String pdesc;
+    private boolean delFlag;
+
+    @Builder.Default
+    private List<MultipartFile> files = new ArrayList<>();
+    //실제 파일 데이터 
+    @Builder.Default
+    private List<String> uploadFileNames = new ArrayList<>();
+    //문자열로만 보관
+}
