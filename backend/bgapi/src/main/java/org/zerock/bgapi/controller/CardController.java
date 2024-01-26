44792d99a2fc61b5121a85ff184730f2904aee02
soyoon26 <barcardi26@gmail.com>
2024-01-26 16:06:49 +0900

    add CardServeltConfig

--- backend/bgapi/src/main/java/org/zerock/bgapi/controller/CardController.java
+++ backend/bgapi/src/main/java/org/zerock/bgapi/controller/CardController.java
@@ -12,20 +12,15 @@ import org.zerock.bgapi.util.CardFileUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequiredArgsConstructor
 @Log4j2
 @RequestMapping("/api/cards")
-
-
 public class CardController {
     private final CardFileUtil fileUtil;
 
-    @PostMapping("/")
+    @PostMapping("/api/cards/")
     public Map<String, String> register(CardDTO cardDTO) {
         log.info("rgister: "+ cardDTO);
         List<MultipartFile> files = cardDTO.getFiles();
