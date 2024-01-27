38d5ffd957eb57f7b2b479f68d6abd702519d4c8
soyoon26 <barcardi26@gmail.com>
2024-01-27 18:00:20 +0900

    update cardController

--- backend/bgapi/src/main/java/org/zerock/bgapi/controller/CardController.java
+++ backend/bgapi/src/main/java/org/zerock/bgapi/controller/CardController.java
@@ -3,8 +3,12 @@ package org.zerock.bgapi.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +17,8 @@ import org.zerock.bgapi.util.CardFileUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+// import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +28,7 @@ public class CardController {
     private final CardFileUtil fileUtil;
 
     @PostMapping("/")
-    public Map<String, String> register(@RequestBody CardDTO cardDTO) {
+    public Map<String, String> register(CardDTO cardDTO) {
         System.out.println("왜안되느뇨");
         log.info("rgister: "+ cardDTO);
         List<MultipartFile> files = cardDTO.getFiles();
@@ -32,4 +38,9 @@ public class CardController {
         log.info("Upload file names: {}",uploadFileNames);
         return Map.of("RESULT", "SUCCESS");
     }
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName) {
+        return fileUtil.getFile(fileName);
+    }
+    
 }
