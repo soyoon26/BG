4050d79d8c2739632f42505925d7c0ee9f71fb8a
soyoon26 <barcardi26@gmail.com>
2024-01-27 18:46:22 +0900

    update comment

--- backend/bgapi/src/main/java/org/zerock/bgapi/domain/comment.java
+++ backend/bgapi/src/main/java/org/zerock/bgapi/domain/comment.java
@@ -1,5 +1,23 @@
 package org.zerock.bgapi.domain;
 
-public class comment {
-    
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_comment")
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //고유한 PK
+    private Long tno;
+    private String title;
+    private String writer;
+    private boolean complete;
+    private LocalDate dueDate;
 }
