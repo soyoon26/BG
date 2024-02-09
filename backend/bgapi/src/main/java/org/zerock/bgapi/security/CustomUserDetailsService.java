09e8a4d32ab04dc30da184d2bec6a6b27fb3b217
soyoon26 <barcardi26@gmail.com>
2024-02-09 23:58:16 +0900

    change userService

--- backend/bgapi/src/main/java/org/zerock/bgapi/security/CustomUserDetailsService.java
+++ backend/bgapi/src/main/java/org/zerock/bgapi/security/CustomUserDetailsService.java
@@ -1,9 +1,13 @@
 package org.zerock.bgapi.security;
 
+import java.util.stream.Collectors;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.zerock.bgapi.domain.Member;
+import org.zerock.bgapi.dto.MemberDTO;
 import org.zerock.bgapi.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +25,21 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("--------------loadUserByUsername-------------------");
+        Member member = memberRepository.getWithRoles(username);
+        if(member == null){
+            throw new UsernameNotFoundException("Not Found");
+        }
 
-        return null;
+        MemberDTO memberDTO = new MemberDTO(
+            member.getEmail(),
+            member.getPw(),
+            member.getNickname(),
+            member.isSocial(),
+            member.getMemberRoleList()
+                .stream()
+                .map(memberRole -> memberRole.name()).collect(Collectors.toList()));
+        log.info(memberDTO);
+        return memberDTO;    
+        }
     }
-}
+
