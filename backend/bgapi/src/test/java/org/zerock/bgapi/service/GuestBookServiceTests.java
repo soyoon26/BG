167e3dacb102151e574309315e03f3858424d255
soyoon26 <barcardi26@gmail.com>
2024-02-12 12:16:10 +0900

    test register

---
+++ backend/bgapi/src/test/java/org/zerock/bgapi/service/GuestBookServiceTests.java
@@ -0,0 +1,30 @@
+package org.zerock.bgapi.service;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.bgapi.dto.GuestBookDTO;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class GuestBookServiceTests {
+    @Autowired
+    private GuestBookService guestbookService;
+    
+    @Test
+    public void testRegister() {
+        GuestBookDTO guestbookDTO = GuestBookDTO.builder()
+        .content("테스트 진행")
+        .writer("superuser")
+        .dueDate(LocalDate.of(2024,02,12))
+        .build();
+
+        Long no = guestbookService.register(guestbookDTO);
+
+        log.info("NO: " + no);
+    }
+}
