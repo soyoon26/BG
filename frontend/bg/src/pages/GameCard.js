58e53450e78527d173e57e05ef291c6f6963108c
soyoon26 <barcardi26@gmail.com>
2024-03-19 11:37:10 +0900

    apply randomCards

--- frontend/bg/src/pages/GameCard.js
+++ frontend/bg/src/pages/GameCard.js
@@ -6,9 +6,11 @@ import "./GameCard.css";
 const GameNumber = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const step = searchParams.get("step");
+  const step = parseInt(searchParams.get("step")); //정수로 바꿔줌
   const [fileData, setFileData] = useState(null); //링크
-  const [usedPictureCards, setUesdPictureCards] = useState([]); //사용된 그림 카드 배열
+  const [pictureCard, setPictureCard] = useState(null);
+  const [numberCard, setNumberCard] = useState(null);
+  const [usedPictureCards, setUsedPictureCards] = useState([]); //사용된 그림 카드 배열
   const [usedNumberCards, setUsedNumberCards] = useState([]); //사용된 숫자 카드 배열
 
   const pictureCards = [
@@ -43,29 +45,69 @@ const GameNumber = () => {
   const numberCards = Array.from({ length: 10 }, (_, i) => (i + 1).toString()); //사용하지 않은 숫자카드 배열
 
   useEffect(() => {
-    // 파일 데이터 가져오기
-    const fetchData = async () => {
+    const fetchPictureCard = async () => {
+      //그림카드 비동기로 가져오기
       try {
-        const imageUrl = await getOne("1.png"); // 파일 이름을 지정해야 함
-        console.log(imageUrl, "카드 가져오기 성공"); // 여기에 추가
-        setFileData(imageUrl);
+        let randomPictureCard;
+        do {
+          //do-while 실행 뒤 조건 검사
+          randomPictureCard =
+            pictureCards[Math.floor(Math.random() * pictureCards.length)];
+        } while (usedPictureCards.includes(randomPictureCard));
+        //랜덤그림카드 구하기
+        setUsedPictureCards((prevUsedPictureCards) => [
+          ...prevUsedPictureCards,
+          randomPictureCard,
+        ]);
+        //사용한 그림카드에 넣기, 불변성을 위해
+        const imageUrl = await getOne(`${randomPictureCard}.png`);
+        console.log(imageUrl, "그림카드 가져오기 성공");
+        setPictureCard(imageUrl);
       } catch (error) {
-        console.error("Error fetching file data:", error);
+        console.error("Error fetching picture card data:", error);
       }
     };
-    fetchData(); // 컴포넌트가 처음 렌더링될 때 파일 데이터를 가져오도록 함
-  }, []);
+
+    const fetchNumberCard = async () => {
+      //숫자카드 가져오기
+      try {
+        let randomNumberCard;
+        do {
+          randomNumberCard =
+            numberCards[Math.floor(Math.random() * numberCards.length)];
+        } while (usedNumberCards.includes(randomNumberCard));
+
+        setUsedNumberCards((prevUsedNumberCards) => [
+          ...prevUsedNumberCards,
+          randomNumberCard,
+        ]);
+
+        const imageUrl = await getOne(`${randomNumberCard}.png`);
+        console.log(imageUrl, "숫자카드 가져오기 성공");
+        setNumberCard(imageUrl);
+      } catch (error) {
+        console.error("Error fetching number card data:", error);
+      }
+    };
+
+    const intervalId = setInterval(() => {
+      fetchPictureCard();
+      fetchNumberCard();
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [usedPictureCards, usedNumberCards]);
 
   return (
     <div>
       <p className="step-info">{step}단계 게임</p>
 
       <div className="card-container">
-        {fileData && (
-          <img className="match-card" src={fileData} alt="그림카드" />
+        {pictureCard && (
+          <img className="match-card" src={pictureCard} alt="그림카드" />
         )}
-        {fileData && (
-          <img className="match-card" src={fileData} alt="숫자카드" />
+        {numberCard && (
+          <img className="match-card" src={numberCard} alt="숫자카드" />
         )}
       </div>
     </div>
