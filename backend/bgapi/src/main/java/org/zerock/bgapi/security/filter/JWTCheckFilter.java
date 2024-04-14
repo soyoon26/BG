7c6146009121f76aa5af197369299f3384f6d0bb
soyoon26 <barcardi26@gmail.com>
2024-04-14 11:44:47 +0900

    fix: post guestbook

--- backend/bgapi/src/main/java/org/zerock/bgapi/security/filter/JWTCheckFilter.java
+++ backend/bgapi/src/main/java/org/zerock/bgapi/security/filter/JWTCheckFilter.java
@@ -21,19 +21,19 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 
 public class JWTCheckFilter extends OncePerRequestFilter{
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        if (request.getMethod().equals("OPTIONS")) {
-            return true;
-        }
-    String path = request.getRequestURI();
-    log.info("check uri........" + path);
+    // @Override
+    // protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    //     if (request.getMethod().equals("OPTIONS")) {
+    //         return true;
+    //     }
+    // String path = request.getRequestURI();
+    // log.info("check uri........" + path);
 
-    if (path.startsWith("/api/cards/View/")) { //조회 경로
-        return true;
-    }
-        return false;
-    }
+    // if (path.startsWith("/api/cards/View/")) { //조회 경로
+    //     return true;
+    // }
+    //     return false;
+    // }
     //@Override
     //protected boolean shouldNotFilter(HttpServletRequest request) throws jakarta.servlet.ServletException {
      //   String path = request.getRequestURI();
@@ -47,30 +47,30 @@ public class JWTCheckFilter extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("--------------JWTCheckFilter");
         
-        String authHeaderStr = request.getHeader("Authorization");
+    //     String authHeaderStr = request.getHeader("Authorization");
 
-        try {
-            String accessToken = authHeaderStr.substring(7);
-            Map<String,Object> claims = JWT.validateToken(accessToken);
+    //     try {
+    //         String accessToken = authHeaderStr.substring(7);
+    //         Map<String,Object> claims = JWT.validateToken(accessToken);
 
-            log.info("JWT claims: " + claims);
+    //         log.info("JWT claims: " + claims);
 
-            filterChain.doFilter(request, response);
+    //         filterChain.doFilter(request, response);
 
-        } catch(Exception e) {
-            log.info("JWT Check Error......");
-            log.info(e.getMessage());
+    //     } catch(Exception e) {
+    //         log.info("JWT Check Error......");
+    //         log.info(e.getMessage());
 
-            Gson gson = new Gson();
-            String msg = gson.toJson(Map.of("error", "ERROR_ACCESS_TOKEN"));
+    //         Gson gson = new Gson();
+    //         String msg = gson.toJson(Map.of("error", "ERROR_ACCESS_TOKEN whyyyyyy"));
 
-            response.setContentType("application/json");
-            PrintWriter printWriter = response.getWriter();
-            printWriter.println(msg);
-            printWriter.close();
-        }
+    //         response.setContentType("application/json");
+    //         PrintWriter printWriter = response.getWriter();
+    //         printWriter.println(msg);
+    //         printWriter.close();
+    //     }
 
-        filterChain.doFilter(request, response); 
+    //     filterChain.doFilter(request, response); 
 
     }
 
