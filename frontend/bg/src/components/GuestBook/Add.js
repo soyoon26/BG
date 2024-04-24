0710697ec157bffa29fc1da331626c7d5f5f3d1d
soyoon26 <barcardi26@gmail.com>
2024-04-25 00:33:01 +0900

    feat: add modal

--- frontend/bg/src/components/GuestBook/Add.js
+++ frontend/bg/src/components/GuestBook/Add.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { postAdd } from "../../api/guestBookApi";
-
+import Modal from "../Common/Modal";
 const today = new Date();
 const initState = {
   content: "",
   writer: "",
   date: today.toISOString().substring(0, 10),
 };
-
 const Add = () => {
+  const [result, setResult] = useState(null);
   const [guestBook, setGuestBook] = useState({ ...initState });
   const handleChange = (e) => {
     guestBook[e.target.name] = e.target.value;
@@ -19,14 +19,24 @@ const Add = () => {
       .then((result) => {
         console.log(result);
         setGuestBook({ ...initState });
+        setResult(result);
+        console.log(result.NO);
       })
       .catch((e) => {
         console.error(e);
       });
   };
+  const closeModal = () => {
+    setResult(null);
+  };
 
   return (
     <div>
+      {result ? (
+        <Modal content={"등록되었습니다."} callbackFn={closeModal} />
+      ) : (
+        <></>
+      )}
       <div>방명록</div>
       <input
         name="content"
