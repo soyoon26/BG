ab77f45ead913db87bdc815d2f6f4c427b3836d3
soyoon26 <barcardi26@gmail.com>
2024-05-01 19:56:37 +0900

    feat: add menuInMap

--- frontend/bg/src/pages/MapPage.js
+++ frontend/bg/src/pages/MapPage.js
@@ -2,6 +2,7 @@ import MapSearch from "../components/Map/MapSearch";
 import SelectMenu from "../components/Button/SelectMenu";
 import back from "../images/back_ivory.png";
 import "./MapPage.css";
+import MenuBar from "../components/Common/MenuBar";
 const MapPage = () => {
   const backStyles = {
     backgroundImage: `url(${back})`,
@@ -10,13 +11,12 @@ const MapPage = () => {
   };
   return (
     <div style={backStyles}>
+      <MenuBar />
       <div className="map-container">
         <div className="map-txt">
           전문적인 진단을 받고 싶다면 가까운 병원을 검색해보아요 🏥
         </div>
-        <div className="select-container-m">
-          <SelectMenu />
-        </div>
+
         <MapSearch />
       </div>
     </div>
